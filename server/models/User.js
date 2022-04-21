20d874a80d78ddfac458b70e2995ff36c2a1a58f
Mercury06 <zetmail@ukr.net>
2022-04-21 21:49:01 +0300

    user model modified

--- server/models/User.js
+++ server/models/User.js
@@ -3,8 +3,12 @@ const {Schema, model} = require ("mongoose")
 const User = new Schema ({
     username: {type: String, required: true, unique: true},
     password: {type: String, required: true},
+    phone: {type: String},
+    email: {type: String},
+    //dateOfBirth: {type: Date},
+    age: {type: Number}, //? заменить на function от dateOfBirth
     avatar: {type: String},
-    rate: {type: Number},
+    rate: {type: Number, default: 5},
     feedbacks: [{type: String}],    
     roles: [{type: String, ref: 'Role'}] //указываем ссылку на сущность роли
 }, {timestamps: true}) //User.createdAt; User.updatedAt
