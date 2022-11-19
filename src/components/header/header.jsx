bd6d8ba9a6240e4e9e9fceafeb56363c8a1a75b3
Mercury06 <zetmail@ukr.net>
2022-11-19 16:22:52 +0300

    commit3

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,36 +1,47 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import './header.css'
-//import Logo from '../Icons/ethers.svg'
-import {NavLink} from "react-router-dom";
-import {logout} from "../../reducers/userReducer"
+import './header.css';
+import { NavLink } from 'react-router-dom';
+import { logout } from '../../reducers/userReducer';
 //import {showLoader} from "../../reducers/appReducer";
 //import {getFiles, searchFiles} from "../../actions/file";
-
+//import Logo from '../Icons/ethers.svg'
 
 const Header = () => {
+  const isAuth = useSelector((state) => state.user.isAuth);
+  const user = useSelector((state) => state.user.currentUser);
+  const login = user.username;
+  // const currentDir = useSelector( state => state.files.currentDir)
+  const dispatch = useDispatch();
 
-    const isAuth = useSelector( state => state.user.isAuth )
-    const user = useSelector( state => state.user.currentUser )
-    const login = user.username;
-    // const currentDir = useSelector( state => state.files.currentDir)
-    const dispatch = useDispatch()
-       
+  return (
+    <div className="header">
+      <div className="container">
+        <div className="header__header"></div>
 
-    return (
-        <div className="header">
-        
-            <div className="container">
-              
-                <div className="header__header"></div>    
-                       
-                    { !isAuth && <div className="header__login"><NavLink to="/login">Login</NavLink></div>}                
-                    { !isAuth && <div className="navbar__registration"><NavLink to="/registration">Registration</NavLink></div>}                    
-                    { isAuth && <> <div className="navbar__login" title='logout' onClick={() => dispatch(logout()) }><NavLink to="/login"><b>{login}</b></NavLink></div></>}
-                     
+        {!isAuth && (
+          <div className="header__login">
+            <NavLink to="/login">Login</NavLink>
+          </div>
+        )}
+        {!isAuth && (
+          <div className="navbar__registration">
+            <NavLink to="/registration">Registration</NavLink>
+          </div>
+        )}
+        {isAuth && (
+          <>
+            {' '}
+            <div className="navbar__login" title="logout" onClick={() => dispatch(logout())}>
+              <NavLink to="/login">
+                <b>{login}</b>
+              </NavLink>
             </div>
-        </div>
-    );
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Header;
