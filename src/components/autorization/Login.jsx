8cff657bbf1aff3c676c27fd8abe5d41e91e1334
Mercury06 <zetmail@ukr.net>
2022-12-28 18:55:56 +0300

    commit3

--- src/components/autorization/Login.jsx
+++ src/components/autorization/Login.jsx
@@ -1,38 +1,41 @@
-import React, { useState} from "react";
-import { useDispatch } from "react-redux";
-import { login } from "../api/actions";
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { login } from '../api/actions';
 //import { login } from "./../../actions/users";
-//import {NavLink} from "react-router-dom";
-import "./authorization.css";
-
-
+import './authorization.css';
 
 const Login = () => {
-   
-    const[form, setForm] = useState({ username: "", password: "" })
-    //const[response, setResponse] = useState('')        
-   
-    const changeHandler = event => {
-        setForm({ ...form, [event.target.name]: event.target.value })
-       // console.log(form)
-    }
-    const dispatch = useDispatch();   
+  const [form, setForm] = useState({ username: '', password: '' });
+  //const[response, setResponse] = useState('')
 
-    return (
-        <div className='authorization'>
-        
-            <div className='authorization__header'><h4>Login</h4></div>            
-           
-                <input onChange={changeHandler} type="text" name="username" placeholder="Username" autoFocus="autofocus"/><br></br>
-         
-                <input  onChange={changeHandler} type="password" name="password" placeholder="Password" />  <br></br>       
-          
-                <button className="authorization__btn" onClick={() => dispatch(login({...form}))}>Sign in</button>
-                {/* <button className="authorization__btn" onClick={() => login({...form})}>Sign in</button> */}
-                {/* { response && <div className="authorization_response">{response}</div>} 
-                <div><NavLink to="/registration">Don`t have an account? Register</NavLink></div> */}
-        </div>
-    );
+  const changeHandler = (event) => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+    // console.log(form)
+  };
+  const dispatch = useDispatch();
+
+  return (
+    <div className="authorization">
+      <div className="authorization__header">
+        <h4>Login</h4>
+      </div>
+      <input
+        onChange={changeHandler}
+        type="text"
+        name="username"
+        placeholder="Username"
+        autoFocus="autofocus"
+      />
+      <br></br>
+      <input onChange={changeHandler} type="password" name="password" placeholder="Password" />{' '}
+      <br></br>
+      <button className="authorization__btn" onClick={() => dispatch(login({ ...form }))}>
+        Sign in
+      </button>
+      {/* <button className="authorization__btn" onClick={() => login({...form})}>Sign in</button> */}
+      {/* { response && <div className="authorization_response">{response}</div>} */}
+    </div>
+  );
 };
 
 export default Login;
